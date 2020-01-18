ac8d24ee6d3dd54de0d5c929df8f5bb001a914f0
yc424k <yc424k@gmail.com>
2020-01-18 13:27:56 +0000

    20200118

---
+++ number.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<hmtl>
+	<body>
+		<h1>NUMBER & Arithmetic Operator</h1>
+		<h2>1+1</h2>
+		<?php
+			echo 1+1;
+		?>
+		<h2>2-1</h2>
+		<?php
+			echo 2-1;
+		?>
+	</body>
+</hmtl>
